8510caf8e2f4c5fcd3a0b300be75bd01890ab51b
Vong Hoang Vinh <vonghoangvinh1989@gmail.com>
2022-10-28 17:31:01 +0700

    fix some code in ProductList in case no products

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -4,11 +4,13 @@ import ProductCard from "./ProductCard";
 function ProductList({ products, loading }) {
   return (
     <Grid container spacing={2} mt={1}>
-      {products.map((product, index) => (
-        <Grid key={product.id} item xs={6} md={4} lg={3}>
-          <ProductCard product={product} />
-        </Grid>
-      ))}
+      {products
+        ? products.map((product, index) => (
+            <Grid key={product.id} item xs={6} md={4} lg={3}>
+              <ProductCard product={product} />
+            </Grid>
+          ))
+        : "No Data Found"}
     </Grid>
   );
 }
