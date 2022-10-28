8d638c9b29814dd8715d976662b8ba32649f6cb5
Vong Hoang Vinh <vonghoangvinh1989@gmail.com>
2022-10-28 15:52:09 +0700

    update code to fix on server

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import App from "./App";
+import { createRoot } from "react-dom/client";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// create container
+const container = document.querySelector("#root");
+const root = createRoot(container);
+root.render(<App />);
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -38,7 +38,7 @@ function HomePage() {
         setError("");
       } catch (error) {
         console.log(error);
-        setError(error.message);
+        setError("No Data Found.");
       }
       setLoading(false);
     };
@@ -102,7 +102,7 @@ function applyFilter(products, filters) {
   }
 
   // FILTER PRODUCTS
-  if (filters.gender.length > 0) {
+  if (filters.gender?.length > 0) {
     filteredProducts = products.filter((product) =>
       filters.gender.includes(product.gender)
     );
